df66049047d54c428ede5a62b6fa00f4d7420dca
Lucius <azhenghendali@gmail.com>
2025-08-11 16:59:41 +0800

    网站添加clarity用户行为监控

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,19 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "st33hcj907");
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="//www.buzzcutfilter.app" />
